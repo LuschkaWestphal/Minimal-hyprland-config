d0e303476de18fc0a50efe0ff72560c75aaf4b80
wernicke23 <mateanagy2002@gmail.com>
2026-02-20 14:46:27 +0100

    Add files via upload

---
+++ config/fish/conf.d/uv.env.fish
@@ -0,0 +1,2 @@
+
+source "$HOME/.var/app/org.kde.krita/data/krita/ai_diffusion/server/uv/env.fish"
---
+++ config/fish/config.fish
@@ -0,0 +1,10 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+set fish_cursor_default block
+
+# starship init fish | source
+
+set -g fish_greeting
+function fish_greeting
+    # do nothing
+end
